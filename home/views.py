1a7985ead879f54b108772b5afeccb8d6269bf59
saleh ebrahimi <saleh.ebrahimi0404@gmail.com>
2023-09-04 05:55:16 +0330

    variable,filter,tag,comment syntax in html page used

--- home/views.py
+++ home/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 
 def sey_hello(request):
-    return render(request, 'hello.html')
+    person = {'name':'saleh'}
+    return render(request, 'hello.html', context=person)
 
 def home(request):
     return render(request, 'home.html')
